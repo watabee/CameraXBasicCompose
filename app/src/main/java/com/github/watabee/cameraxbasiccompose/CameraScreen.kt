538c65abb26693b3b1b8a0817e4ab7eb31a2ad21
watabee <watabee.dev@gmail.com>
2022-08-20 19:13:41 +0900

    Added flush animation

--- app/src/main/java/com/github/watabee/cameraxbasiccompose/CameraScreen.kt
+++ app/src/main/java/com/github/watabee/cameraxbasiccompose/CameraScreen.kt
@@ -2,6 +2,7 @@ package com.github.watabee.cameraxbasiccompose
 
 import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -10,10 +11,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -21,12 +28,17 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun CameraScreen() {
     Box(modifier = Modifier.fillMaxSize()) {
         val photoFileController = rememberPhotoFileController()
         val cameraViewState = rememberCameraViewState(photoFileController)
+        val coroutineScope = rememberCoroutineScope()
+        // Flush animation.
+        var alpha by remember { mutableStateOf(0f) }
 
         CameraView(
             modifier = Modifier.fillMaxSize(),
@@ -47,7 +59,17 @@ fun CameraScreen() {
                 modifier = Modifier
                     .align(Alignment.BottomCenter)
                     .padding(bottom = 80.dp),
-                onClick = cameraViewState::takePicture
+                onClick = {
+                    cameraViewState.takePicture()
+                    // Flush animation.
+                    coroutineScope.launch {
+                        delay(100L)
+                        alpha = 1f
+                        delay(50L)
+                    }.invokeOnCompletion {
+                        alpha = 0f
+                    }
+                }
             )
         }
 
@@ -57,6 +79,14 @@ fun CameraScreen() {
                 .padding(bottom = 92.dp, end = 32.dp),
             photoUri = photoFileController.lastSavedPhotoUri,
             onClick = {}
+        )
+
+        // Flush animation.
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer(alpha = alpha)
+                .background(color = Color.White)
         )
     }
 }
